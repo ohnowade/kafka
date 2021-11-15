11902f7ee5c515c2df3112298d154f28064a3f69
ohnowade <ctan46@wisc.edu>
2021-11-14 16:47:57 -0800

    modify style to pass tests

--- clients/src/main/java/org/apache/kafka/common/FeedbackQueue.java
+++ clients/src/main/java/org/apache/kafka/common/FeedbackQueue.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.common;
 
 import org.apache.kafka.common.utils.Utils;
@@ -62,15 +78,15 @@ public class FeedbackQueue {
                 topQueue = bottomQueue;
                 bottomQueue = new ArrayList<>();
             } else {
-                topQueue.set(prevPartitionIndex, topQueue.get(size-1));
-                topQueue.remove(size-1);
+                topQueue.set(prevPartitionIndex, topQueue.get(size - 1));
+                topQueue.remove(size - 1);
             }
             Integer random = Utils.toPositive(ThreadLocalRandom.current().nextInt());
             prevPartitionIndex = random % topQueue.size();
             prevPartition = topQueue.get(prevPartitionIndex);
             System.out.printf("The next partition chosen is %d.\n", prevPartition);
         }
-        counter.compute(prevPartition, (k, v)->v+recordSize);
+        counter.compute(prevPartition, (k, v) -> v + recordSize);
         rs = prevPartition;
         System.out.printf("Partition %d is chosen with %d bytes assigned to it.\n", rs, recordSize);
         lock.unlock();
